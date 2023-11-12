17bf4cf348305a75d6e9026b2fcdc7689789a903
ben64103 <benabolade@gmail.com>
2023-11-12 19:32:35 +0100

    done with part1 uncafe

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,23 +1,10 @@
-import Header from "./Header";
-import Content from "./Content";
-import Total from "./Total";
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
 
 const App = () => {
-  const course = {
-    name: "Half Stack Application development",
-    parts: [
-      { name: "Fundamentals of React", exercises: 10 },
-      { name: "Using props to pass data", exercises: 7 },
-      { name: "State of a component", exercises: 14 },
-    ],
-  };
   return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
--- part1/courseinfo/src/Content.jsx
+++ part1/courseinfo/src/Content.jsx
@@ -16,5 +16,56 @@ const Part = props => {
   );
 };
 
-export default Content
 
+const App = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  // const [clicks, setClicks] = useState({
+  //   left: 0, right: 0,
+  // })
+
+  const History = props => {
+    if (props.allClicks.length === 0) {
+      return <div>the app is used by presssing the buttons</div>;
+    }
+    return <div>button press History: {props.allClicks.join(' ')}</div>;
+  };
+  
+  const Button = (props) => {
+    console.log(props)
+    const { handleClick, text } = props
+   return (
+    <button onClick={handleClick}>{text}</button>
+   )
+  }
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'));
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    setTotal(updatedLeft + right);
+    // setClicks({ ...clicks, left: clicks.left + 1 })
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    setRight(right + 1);
+    setTotal(left + right);
+    // setClicks({ ...clicks, right: clicks.right + 1 })
+  };
+
+  return (
+    <div>
+      {left}
+        <Button handleClick={handleLeftClick} text={'left'}/>
+        <Button handleClick={handleRightClick} text={'right'}/>
+      {right}
+
+      <History allClicks={allClicks} />
+    </div>
+  );
+};
+export default Content
