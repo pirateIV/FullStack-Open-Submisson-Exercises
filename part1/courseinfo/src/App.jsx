762fbf32cfe51564f116349d970bec6a435bd997
ben64103 <benabolade@gmail.com>
2023-11-13 13:51:54 +0100

    ...part 1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,10 +1,49 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react'
-
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
 
-export default App
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises1}
+      />
+    </div>
+  );
+};
+
+const Header = props => <h1>{props.course}</h1>;
+
+const Content = props => (
+  <>
+    <Part part={props.part1} exercises={props.exercises1} />
+    <Part part={props.part2} exercises={props.exercises2} />
+    <Part part={props.part3} exercises={props.exercises3} />
+  </>
+);
+const Part = ({ part, exercises }) => (
+  <p>
+    {part} {exercises}
+  </p>
+);
+
+const Total = ({ exercises1, exercises2, exercises3 }) => (
+  <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+);
+
+export default App;
