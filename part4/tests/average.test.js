446bd85d8d687a99171f113638157658e88774f7
ben64103 <benabolade@gmail.com>
2024-02-10 03:06:40 +0100

    testing backend with eslint

--- part4/tests/average.test.js
+++ part4/tests/average.test.js
@@ -4,4 +4,8 @@ describe('average', () => {
   test('of one value is the value of itself', () => {
     expect(average([1])).toBe(1);
   });
+
+  test('of many is calculated right', () => {
+    expect(average([1, 2, 3, 4, 5, 6])).toBe(3.5);
+  });
 });
