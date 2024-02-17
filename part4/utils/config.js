8de30bc4cb1f4414ab79b67b548a9bc238f32b31
ben64103 <benabolade@gmail.com>
2024-02-17 11:55:12 +0100

    updated dist folder for part4 backend phonebook app

--- part4/utils/config.js
+++ part4/utils/config.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 
-// eslint-disable-next-line no-undef
-const { PORT, MONGO_URI } = process.env;
+const PORT = process.env.PORT;
+
+const MONGODB_URI =
+  process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI;
 
 module.exports = { PORT, MONGO_URI };
